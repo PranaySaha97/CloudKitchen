6cc781671ba240b18a8813fb5bb57cdc397330e4
PranaySaha97 <saha.pranay97@gmail.com>
2020-08-09 19:10:27 +0530

    ImageHandler Updated

--- cloudkitchen-ws/utilities/custImageHandler.js
+++ cloudkitchen-ws/utilities/custImageHandler.js
@@ -3,19 +3,14 @@ let sharp = require('sharp');
 let imageHandler = async (req) => {
     let filename = new Date().toDateString() + '-' + req.file.originalname;
     filename = filename.split(' ').join('-'); // removes empty space in file name and replaces with '-'
-
     // await is used as sharp is asynchronous
     await sharp(req.file.buffer)
         .resize({
             width: 615,
             height: 350
         })
-<<<<<<< HEAD:cloudkitchen-ws/utilities/custImageHandler.js
-        .toFile('./uploads/images/customer/' + filename); // sharp is used to compress the image and store in the location
-=======
         .withMetadata()
-        .toFile('./uploads/images/' + filename); // sharp is used to compress the image and store in the location
->>>>>>> 8cccc118ced1ab52570ba0f685c885425f38695d:cloudkitchen-ws/utilities/imageHandler.js
+        .toFile('./uploads/images/customer/' + filename); // sharp is used to compress the image and store in the location
 }
 
 module.exports = imageHandler;
