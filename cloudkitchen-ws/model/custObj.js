6314f15737cfb6d7ca63437cae2ea1b7515ab1fe
Shatakshi Srivastava <shatakshisri@gmail.com>
2020-08-13 19:31:09 +0530

    Changed admin

--- cloudkitchen-ws/model/custObj.js
+++ cloudkitchen-ws/model/custObj.js
@@ -1,10 +1,6 @@
 class Customer {
-    constructor(obj){
-<<<<<<< HEAD
-        this.customerId="";
-=======
+    constructor(obj){        
         this.customerId='';
->>>>>>> 75d0851e003042798a9a880d73c1c7ce2b6c71f2
         this.userName= obj.userName;
         this.password=obj.password;
         this.name= obj.name;
