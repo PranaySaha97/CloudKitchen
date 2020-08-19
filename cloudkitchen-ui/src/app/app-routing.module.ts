628e61692ba5d8a9c43b888d43ae215696a74b78
Shatakshi Srivastava <shatakshisri@gmail.com>
2020-08-19 21:22:18 +0530

    comit

--- cloudkitchen-ui/src/app/app-routing.module.ts
+++ cloudkitchen-ui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { CustomerHomeComponent } from './customer-home/customer-home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 
 const routes: Routes = [
   {path: '', component: CustomerHomeComponent},
+  {path: 'admin', component: AdminHomeComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
