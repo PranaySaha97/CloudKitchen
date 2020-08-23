858d89a45bb4fadc60e0ae0c83d709ece67f102d
Shatakshi Srivastava <shatakshisri@gmail.com>
2020-08-24 00:11:20 +0530

    admin

--- cloudkitchen-ui/src/app/app-routing.module.ts
+++ cloudkitchen-ui/src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import {Routes, RouterModule} from '@angular/router';
 import {AdminHomeComponent} from './admin-home/admin-home.component';
 import {DeliveryPersonLoginComponent} from './delivery-person-login/delivery-person-login/delivery-person-login.component';
 import { DeliveryPersonRegisterComponent } from './delivery-person-register/delivery-person-register/delivery-person-register.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminOrdersComponent } from './admin-orders/admin-orders.component';
+import { AdminDeliveryPersonsComponent } from './admin-delivery-persons/admin-delivery-persons.component';
+import { AdminCustomersComponent } from './admin-customers/admin-customers.component';
+import { AdminRestaurantsComponent } from './admin-restaurants/admin-restaurants.component';
 
 const routes: Routes = [
     {
@@ -16,6 +21,21 @@ const routes: Routes = [
     }, {
         path: 'admin',
         component: AdminHomeComponent
+    },{
+        path: 'admin/login',
+        component: AdminLoginComponent
+    },{
+        path: 'admin/orders',
+        component: AdminOrdersComponent
+    },{
+        path: 'admin/deliverypersons',
+        component: AdminDeliveryPersonsComponent
+    },{
+        path: 'admin/restaurants',
+        component: AdminRestaurantsComponent
+    },{
+        path: 'admin/customers',
+        component: AdminCustomersComponent
     }, {
         path: 'deliveryperson',
         component: DeliveryPersonLoginComponent
